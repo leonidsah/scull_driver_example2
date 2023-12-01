aa28dd3a069095b33ee53f16610fcc7bd98c167a
leonidsah <37192461+leonidsah@users.noreply.github.com>
2023-12-01 17:24:03 +0300

    Update main.c
    Add ioctl() call for changing scull's offset

--- main.c
+++ main.c
@@ -3,14 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
 
 #define BUFF_SIZE 100
 #define DEV0 "/dev/scull0"
+#define SCULL_IOCTL1 0x7701
 
 int main()
 {
     int fd;
     char ch, write_buf[BUFF_SIZE], read_buf[BUFF_SIZE];
+    int offset;
 
     printf("Устройство %s: \n", DEV0);
 
@@ -20,7 +23,7 @@ int main()
         perror("Ошибка открытия устройства");
         return -1;
     }
-    printf("r - read, w - write\n");
+    printf("r - read, w - write, o - offset\n");
     scanf(" %c", &ch);
 
     switch (ch) {
@@ -37,6 +40,11 @@ int main()
             read_buf[bytes_read] = '\0'; // Добавляем символ конца строки
             printf("scull: %s\n", read_buf); // Выводим прочитанные данные
         }
+        break;
+    case 'o':
+        printf("Введите смещение для чтения/записи: ");
+        scanf("%d", &offset);
+        ioctl(fd, SCULL_IOCTL1, offset); 
         break;
     }
     close(fd);
